5848fa5bf08bb0b8ae60044c6fd7d43390ac210b
Daniel Lee <danielleestat@gmail.com>
2021-01-08 19:09:02 -0800

    Add config

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
+import os
 from src import etl
 import sys
 
 
 def main(targets):
+    autophrase_params = json.load(open('config/autophrase_params.json'))
+    autophrase_params = ' '.join([f'{param}={value}' for param, value in autophrase_params.items()])
+
     if 'etl' in targets:
         etl.main()
+
+    if 'autophrase' in targets:
+        os.system(f'cd AutoPhrase && {autophrase_params} ./auto_phrase.sh && {autophrase_params} ./phrasal_segmentation.sh')
+
+    if 'test' in targets:
+        ...
 
 
 if __name__ == '__main__':
