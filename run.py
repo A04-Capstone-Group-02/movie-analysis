f88acca8ff5b00ea23426d4b6812711897424caf
Daniel Lee <danielleestat@gmail.com>
2021-01-15 16:30:20 -0800

    Add AutoPhrase to ETL

--- run.py
+++ run.py
@@ -1,19 +1,16 @@
 import json
-import os
-from src import etl
+from src.etl import get_data
 import sys
 
 
 def main(targets):
     autophrase_params = json.load(open('config/autophrase_params.json'))
-    autophrase_params = ' '.join([f'{param}={value}' for param, value in autophrase_params.items()])
 
-    if 'etl' in targets:
-        etl.main()
+    if 'data' in targets:
+        get_data(autophrase_params)
 
     if 'train' in targets:
-        # Run AutoPhrase
-        os.system(f'cd AutoPhrase && {autophrase_params} ./auto_phrase.sh && {autophrase_params} ./phrasal_segmentation.sh')
+        ...
 
     if 'test' in targets:
         ...
