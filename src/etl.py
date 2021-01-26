c46c27b0c244906a9ca721ff0638bde890ead308
Daniel Lee <danielleestat@gmail.com>
2021-01-26 09:53:04 -0800

    Update ETL and EDA (#8)

--- src/etl.py
+++ src/etl.py
@@ -22,7 +22,7 @@ def get_data(autophrase_params):
         def sub(value):
             return re.sub(r' [Ll]anguages?', '', value)
 
-        return sorted(np.unique([sub(value) for value in eval(x).values() if is_utf8(value)]))
+        return list(np.unique([sub(value) for value in eval(x).values() if is_utf8(value)]))
 
     def normalize_countries(x):
         return sorted(eval(x).values())
@@ -54,7 +54,7 @@ def get_data(autophrase_params):
                 return 'Social problem'
             return re.sub(' [Ff]ilms?| [Mm]ovies?', '', value)
 
-        return sorted(np.unique([sub(value) for value in eval(x).values()]))
+        return list(np.unique([sub(value) for value in eval(x).values()]))
 
     def clean_summary(summary):
         return (
@@ -95,13 +95,37 @@ def get_data(autophrase_params):
     os.system(f'cd AutoPhrase && {autophrase_params} ./auto_phrase.sh && {autophrase_params} ./phrasal_segmentation.sh')
 
     # Add phrases to df
-    df['phrases'] = pd.read_csv(
+    def extract_highlighted_phrases(segmentation):
+        def is_false_positive(s):
+            s = s.lower()
+            if len(s) == 1:  # Only 1 character
+                return True
+            if s in ['imdb.com']:
+                return True
+            if '|' in s:  # Leftover Wikipedia tags
+                return True
+            if 'featuring' in s:  # E.g. "featuring mithun chakraborty"
+                return True
+            if 'starring' in s:  # E.g. "starring mithun chakraborty"
+                return True
+            return False
+
+        return (
+            segmentation
+            .str.findall(r'<phrase>(.+?)</phrase>')
+            .apply(lambda x: [s.lower() for s in x if not is_false_positive(s)])
+            .apply(np.unique)
+            .apply(list)
+            .values
+        )
+
+    df['phrases'] = extract_highlighted_phrases(pd.read_csv(
         'model/autophrase/segmentation.txt',
         delimiter=r'\n',
         engine='python',
         header=None,
         squeeze=True
-    ).str.findall(r'<phrase>(.+?)</phrase>').apply(lambda x: [s.lower() for s in x]).apply(np.unique).apply(list).values
+    ))
 
     # Export df
     df.to_pickle('data/out/data.pkl')
