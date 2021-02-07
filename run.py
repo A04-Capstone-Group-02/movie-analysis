91d9a50246511f4d535146e81e06544ae517eff8
humiao <humiao@humiao-30415.pods.humiao.svc.cluster.local>
2021-02-06 16:47:03 -0800

    update classification.py and add config file

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import json
 from src.eda import generate_figures
 from src.etl import get_data, download_dataset
 from src.clustering import calc_all_embeddings
+from src.classification import model
 import sys
 
 
@@ -10,6 +11,7 @@ def main(targets):
     autophrase_params = json.load(open('config/autophrase.json'))
     eda_params = json.load(open('config/eda.json'))
     clustering_params = json.load(open('config/clustering.json'))
+    clf_params = json.load(open('config/classification.json'))
 
     if 'download' in targets:
         download_dataset()
@@ -22,7 +24,10 @@ def main(targets):
     
     if 'clustering' in targets:
         calc_all_embeddings(clustering_params)
-
+        
+    if 'classification' in targets:
+        model(clf_params)
+        
     if 'train' in targets:
         ...
 
--- src/classification.py
+++ src/classification.py
@@ -9,57 +9,70 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import f1_score
+import warnings
+warnings.filterwarnings("ignore")
 
-def summary_preprocessing(plot):
-    """preprocessing the movie plot summary through tokenization and removing stopwords"""
+
+
+def baseline_model(plot_summary,genre):
+
+    """preprocessing, vectorization, and classifcation with OneVsRestClassifier and Logistic Regression"""
     #remove punctuation
     tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
-    no_punct = plot.apply(lambda x: tokenizer.tokenize(x))
+    no_punct = plot_summary.apply(lambda x: tokenizer.tokenize(x))
     
     #remove stopwords
     nltk.download('stopwords')
     stop_words = set(nltk.corpus.stopwords.words('english'))
-    no_stopwords = no_punct.apply(lambda x: ' '.join([w for w in x if w not in stop_words]))
+    summary = no_punct.apply(lambda x: ' '.join([w for w in x if w not in stop_words]))
     
-    return no_stopwords
-
-
-
-def model(summary,label):
-    """classify movie genres"""
-    #tf-idf vectorizer
-
+    #label binarizer
+    multilabel_binarizer = sklearn.preprocessing.MultiLabelBinarizer()
+    multilabel_binarizer.fit(genre)
+    label = multilabel_binarizer.transform(genre)
+   
+    #split training and validation set
     xtrain, xval, ytrain, yval = train_test_split(summary, label, test_size=0.2, random_state=9)
     tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=10000)
     xtrain_tfidf = tfidf_vectorizer.fit_transform(xtrain)
     xval_tfidf = tfidf_vectorizer.transform(xval)
     
     #OneVsRest classifier
-    lr = sklearn.linear_model.LogisticRegression()
+    lr = sklearn.linear_model.LogisticRegression(solver = 'liblinear')
     clf = OneVsRestClassifier(lr,n_jobs = 8)
 #     scores = cross_validate(clf, xtrain_tfidf, ytrain, scoring=scoring)
     clf.fit(xtrain_tfidf,ytrain)
     y_pred = clf.predict(xval_tfidf)
     
+    # Predicted label
+    actual_genre = multilabel_binarizer.inverse_transform(yval)
+    predicted_genre = multilabel_binarizer.inverse_transform(y_pred)
+    
     
     #evaluation
     f1 = sklearn.metrics.f1_score(yval, y_pred, average="micro")
-    return multilabel_binarizer.inverse_transform(yval), multilabel_binarizer.inverse_transform(y_pred)
+    
+    e1 = 'percentage of genres that are correctly predicted: '+ str(np.sum([len(set(a).intersection(b)) for a, b in \
+                  zip(pd.Series(predicted_genre), pd.Series(actual_genre))])/sum(genre.apply(len)))
+    e2 = 'percentage of movies that have at least one gnere predicted right: '+str(np.sum([len(set(a).intersection(b))>0 for a, b in\
+                  zip(pd.Series(predicted_genre), pd.Series(actual_genre))])/len(genre))
+    return f1,e1,e2
 
-def tunning_params(summary,label,genres):
-    """parameter tuned classify models""""
-    #tf-idf vectorizer
+def tunning_params(summary,genre):
+    """parameter tuned classify models"""
+    #label binarizer
     multilabel_binarizer = sklearn.preprocessing.MultiLabelBinarizer()
-    multilabel_binarizer.fit(genres)
-    label = multilabel_binarizer.transform(genres)
+    multilabel_binarizer.fit(genre)
+    label = multilabel_binarizer.transform(genre)
+   
+    #split training and validation set
     xtrain, xval, ytrain, yval = train_test_split(summary, label, test_size=0.2, random_state=9)
     tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=10000)
     xtrain_tfidf = tfidf_vectorizer.fit_transform(xtrain)
     xval_tfidf = tfidf_vectorizer.transform(xval)
     
-    #OneVsRest classifier
-    
-
+  
+    #hyperparameter grid search
     parameters = {
         "estimator__penalty": ['l1'],
         "estimator__solver":['newton-cg','lbfgs', 'liblinear', 'sag', 'saga'],
@@ -73,4 +86,20 @@ def tunning_params(summary,label,genres):
     y_pred = model_tunning.best_estimator_.predict(xval_tfidf)
     
     f1 = sklearn.metrics.f1_score(yval, y_pred, average="micro")
-    return model_tunning.best_score_,model_tunning.best_params_, model_tunning.best_estimator_,f1
+    return model_tunning.best_estimator_,f1
+
+def model(config):
+    pikl = pd.read_pickle(config['data'])
+    df = pikl.dropna(subset = ['genres'])
+    summary = df['summary']
+    genre = df['genres']
+    phrase = df['phrases'].apply(lambda x: ' '.join(x))
+    baseline = True
+    if baseline:
+        s = baseline_model(summary,genre)
+        p = baseline_model(phrase,genre)
+    else:
+        s = tunning_params(summary_genre)
+        p = tunning_params(summary_genre)
+    print('model performance using movie plot summary: '+ str(s)+'\n' + 'model performance using phrases: '+str(p))
+        
